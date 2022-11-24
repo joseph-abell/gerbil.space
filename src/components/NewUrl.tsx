678af5314f5bcd68ddc60999b2bd314e4e5449d5
Joe Abell <jsphabll@gmail.com>
2022-11-24 19:17:10 +0000

    move to using vite

--- src/components/NewUrl.tsx
+++ src/components/NewUrl.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from "react";
 import Hashids from "hashids";
 import {
--- src/db.ts
+++ src/db.ts
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL as string,
-  process.env.REACT_APP_SUPABASE_API as string
+  import.meta.env.VITE_SUPABASE_URL as string,
+  import.meta.env.VITE_SUPABASE_API as string
 );
 
 export default supabase;
